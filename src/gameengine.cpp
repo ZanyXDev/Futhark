2a45bad4ec241a53bdbbc3ba5a754645c864162e
zanyxdev <zanyxdev@gmail.com>
2018-06-01 13:48:51 +0300

    add findBlockLines

--- src/gameengine.cpp
+++ src/gameengine.cpp
@@ -11,36 +11,104 @@ GameEngine::GameEngine(QObject *parent) :
     mRunes.insert("A",4);
     mRunes.insert("R",5);
     mRunes.insert("K",6);
+    mRunes.insert("CLEAR_RUNE",98);
+    mRunes.insert("EMPTY_RUNE",99);
 
-    initRunesSet();
-    initRunesSet();
-    initRunesSet();
+    initRunesSet();    
+    qDebug() <<"end GameEngine constructor";
 }
 
 //------------------------- PRIVATE ------------------------
 
 void GameEngine::initRunesSet()
 {
-    cells.clear();
+    gameMap.clear();
+
     for (int i = 0; i <= FIELD_SIZE * FIELD_SIZE; i++){
-        cells.append( QRandomGenerator::global()->bounded(FUTHARK_SIZE) ); // 0..23 runes number
+        gameMap.append( QRandomGenerator::global()->bounded(FUTHARK_SIZE) ); // 0..23 runes number
     }
-     this->printCellsDebug();
+
+    gameMap[0]= 22;
+    gameMap[1]= 22;
+    gameMap[2]= 22;
+
+    qDebug() << "removes cell count:" << findBlockLines();
+
+    this->printCellsDebug();
 }
 
 int GameEngine::getIndex(int row, int col)
-{
+{    
     return col + ( row * FIELD_SIZE ) ;
 }
 
 void GameEngine::printCellsDebug()
 {
-    qDebug() << "--------------------------";
+    int index = 0;
+    qDebug() << "-------------------------- | --------------------------";
     for (int row = 0;  row <  FIELD_SIZE; row++){
-        QString str = QString::number(row).append("=");
+        QString str = QString::number(row).append("=");        
         for (int col = 0;  col <  FIELD_SIZE; col++){
-            str.append(QString::number( cells.at( getIndex( row, col ) ) )  ).append("|");
+            index = getIndex( row, col );
+            str.append(QString::number( gameMap.at( index ) ) ).append("|");
         }
-        qDebug() <<  str;
+        qDebug() <<  str ;
     }
+}
+
+
+int GameEngine::findBlockLines()
+{    
+    int index = 0;
+    // очистим список для записи результатов
+    toRemoveList.clear();
+    for ( int row = 0; row < FIELD_SIZE; row++ ) {
+        for ( int col = 0; col < FIELD_SIZE; col++ ) {
+
+            index = getIndex( row, col );
+
+            if ( gameMap.at(index) == EMPTY_PLACE ) continue;
+
+            // проверяем горизонтальную цепочку
+            if ( col < (FIELD_SIZE - 2) )
+            {
+/*
+                qDebug() << "|" << index   << "|" << gameMap.at(index)
+                         << "|" << index+1 << "|" << gameMap.at(index+1)
+                         << "|" << index+2 << "|" << gameMap.at(index+2);
+*/
+                if ( gameMap.at(index) == gameMap.at(index+1)  &&
+                     gameMap.at(index) == gameMap.at(index+2) )
+                {
+                    // нашли цепочку, запомним
+
+                    toRemoveList.append( setPair(index,gameMap.at(index)) );
+
+                    toRemoveList.append( setPair( index+1, gameMap.at(index+1) ) );
+                    toRemoveList.append( setPair( index+2, gameMap.at(index+2) ) );
+                }
+            }
+            // проверяем вертикальную цепочку
+            if ( row < (FIELD_SIZE - 2) )
+            {
+                if 	( gameMap.at(index) == gameMap.at(index+FIELD_SIZE)  &&
+                      gameMap.at(index) == gameMap.at(index+FIELD_SIZE+FIELD_SIZE) )
+                {
+                    // нашли цепочку, запомним
+                    toRemoveList.append( setPair( index, gameMap.at(index) ) );
+                    toRemoveList.append( setPair( index+FIELD_SIZE, gameMap.at(index+FIELD_SIZE) ) );
+                    toRemoveList.append( setPair( index+FIELD_SIZE+FIELD_SIZE, gameMap.at(index+FIELD_SIZE+FIELD_SIZE) ) );
+                }
+            }
+        }
+    }
+    return toRemoveList.count();
+}
+
+QPair<int,int> GameEngine::setPair(int key, int value)
+{
+    QPair <int,int> pair;
+    pair.first = key;
+    pair.second = value;
+    return pair;
 }
--- src/gameengine.h
+++ src/gameengine.h
@@ -6,9 +6,12 @@
 #include <QMap>
 #include <QDebug>
 #include <QRandomGenerator>
+#include <QPair>
 
 #define FIELD_SIZE 8
 #define FUTHARK_SIZE 24
+#define EMPTY_PLACE	99
+
 
 class GameEngine : public QObject
 {
@@ -22,13 +25,28 @@ signals:
 public slots:
 
 private:
+    QPair <int,int> sRemoveBlock; // index,value
+
     int mFieldSize;
-    QVector <int> cells;
+    QVector <int> gameMap;    
+    QVector <QPair <int,int>> toRemoveList;
+
     QMap <QString,int> mRunes;
 
-    void initRunesSet();
-    int getIndex(int row, int col);
+    void initRunesSet();    
     void printCellsDebug();
+
+    int getIndex(int row, int col);
+
+    /*!
+      \brief Find block chains to remove.
+      \retval int game score
+      \note
+        поиск цепочек блоков для удаления,удаление блоков возвращает приращение счёта игрока.
+     */
+    int findBlockLines();
+    QPair<int,int> setPair(int key, int value);
+
 };
 
 #endif // GAMEENGINE_H
